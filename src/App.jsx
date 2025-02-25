d881ae7bc646e163c75dc3d327823982b3610396
Sheida Rashidi <84582226+SheidaRa@users.noreply.github.com>
2025-02-25 21:56:43 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="min-h-screen max-h-full bg-white flex items-center justify-center">
+      <div className="font-bold text-pink-300 text-3xl">Sheida testing</div>
+    </div>
+  );
+}
