299059c2e632ae318e66f9e1cdd9cbd3d927dc17
Sheida Rashidi <srashidi@macalester.edu>
2025-03-11 12:26:32 -0500

    Update PipelineConfig.jsx

--- src/PipelineConfig.jsx
+++ src/PipelineConfig.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function PipelineConfig() {
   return (
-    <div className="flex-1 p-6">
+    <div className="flex-1 p-4">
       {/* <h3 className="text-xl font-semibold">Pipeline Configuration</h3> */}
-      <div className="bg-white p-4 shadow-md h-full flex justify-center items-center">
+      <div className="bg-white shadow-md h-full flex justify-center items-center border rounded-lg">
         <div className="mermaid w-full">
           {/* graph TD;
           A[Start] --> B{Decision};
